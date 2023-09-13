3d934f6ced7cf18cb7f1446cb33aca014fc8174b
itaysaban <144318090+itaysaban@users.noreply.github.com>
2023-09-13 11:35:55 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import time
+from webdriver_set import driver_setup
+import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+class Test(unittest.TestCase):
---
+++ webdriver_setup.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+url = ''
+
+
+def driver_setup():
+    options = ChromeOptions()
+    service = ChromeService(ChromeDriverManager().install())
+    options.add_experimental_option('detach', True)
+    options.add_argument('--disable-extentions')
+    options.add_argument('--start-maximized')
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+
+if __name__ == '__main__':
+    driver = driver_setup()
+    driver.get(url)
